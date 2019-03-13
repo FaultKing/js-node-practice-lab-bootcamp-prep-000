24b96a3b60fe3ec39561f4aad1b6015354b68dc6
Ioan Scurtu <ioan.scurtu120@gmail.com>
2019-03-13 15:10:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ var height = "170" //because im familar with metrtic not imperial :)
 console.log("Name:")
 console.log(name)
 
-console.log("Height:")
+console.log((parseInt"Height:")
 console.log(height)
